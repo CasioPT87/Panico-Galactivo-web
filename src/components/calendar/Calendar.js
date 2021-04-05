ca7a5234b8ccd69b2908b3c59a8a2e0f7330dfbe
Sesko <sergioibanezmoreno@gmail.com>
2021-04-05 12:51:12 +0100

    Load next months

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,7 +1,7 @@
 import React, { useState }from 'react';
 import moment from 'moment';
-import { useSpring, animated } from 'react-spring'
 import CalendarMonth from './calendar-month/CalendarMonth';
+import Button from './buttons/Button'
 import H from './helper/CalendarHelper';
 import styles from './Calendar.module.css';
 
@@ -16,19 +16,28 @@ export default function() {
   });
 
   const [position, setPosition] = useState('center');
+  const [transition, setTransition] = useState(false);
   const [monthTable, setMonthTable] = useState(moment().month());
 
   const selectDate = () => {
     console.log('we are selecting a date')
   }
 
-  const goTo = (direction) => {
+  const goTo = (e, direction) => {
+    e.preventDefault();
     let positionIndex = POSITIONS.findIndex(pos => pos === position);
     if (positionIndex < 0 || position > (POSITIONS.length - 1)) return;
     if (direction === 'left') positionIndex--;
     if (direction === 'right') positionIndex++;
     if (!!POSITIONS[positionIndex]) {
+      setTransition(true);
       setPosition(POSITIONS[positionIndex]);
+      setTimeout(() => {
+        setTransition(false);
+        setPosition('center');
+        if (direction === 'left') setMonthTable(H.getPreviousMonth(monthTable));
+        if (direction === 'right') setMonthTable(H.getNextMonth(monthTable));
+      }, 600);
     }
   }
 
@@ -37,12 +46,21 @@ export default function() {
     return styles[styleClass];
   }
 
+  const shouldTransition = () => {
+    if (!transition) return null;
+    return styles['c_calendar--transition'];
+  }
+
   return (
     <div className={styles.c_calendar__wrapper}>
-      <div className={calendarWindowStyle()}>
-        <CalendarMonth monthTable={H.getPreviousMonth(monthTable)} selectedDate={selectedDate} selectDate={selectDate} goTo={goTo}/>
-        <CalendarMonth monthTable={monthTable} selectedDate={selectedDate} selectDate={selectDate} goTo={goTo}/>
-        <CalendarMonth monthTable={H.getNextMonth(monthTable)} selectedDate={selectedDate} selectDate={selectDate} goTo={goTo}/>
+      <div className={styles.c_boxes__buttons}>
+        <Button display="<<" goTo={(e) => goTo(e, 'left')}/>
+        <Button display=">>" goTo={(e) => goTo(e, 'right')}/>
+      </div>
+      <div className={`${styles.c_calendar} ${shouldTransition()} ${calendarWindowStyle()}`}>
+        <CalendarMonth monthTable={H.getPreviousMonth(monthTable)} selectedDate={selectedDate} selectDate={selectDate} />
+        <CalendarMonth monthTable={monthTable} selectedDate={selectedDate} selectDate={selectDate} />
+        <CalendarMonth monthTable={H.getNextMonth(monthTable)} selectedDate={selectedDate} selectDate={selectDate} />
       </div>
     </div>
   );
--- src/components/calendar/calendar-month/CalendarMonth.js
+++ src/components/calendar/calendar-month/CalendarMonth.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import H from '../helper/CalendarHelper'
 import Box from '../box/Box';
-import Button from '../buttons/Button'
 import CalendarFrame from '../calendar-frame/CalendarFrame'
-import styles from './CalendarMonth.module.css'
 
-export default function({ goTo, monthTable, selectDate, selectedDate }) {
-
-  const goToMonth = (e, direction) => {
-    e.preventDefault();
-    goTo(direction);
-  }
-
+export default function({ monthTable, selectDate, selectedDate }) {
   return (
     <div>
-      <div className={styles.c_boxes__buttons}>
-        <Button display="<<" goTo={(e) => goToMonth(e, 'left')}/>
-        <Button display=">>" goTo={(e) => goToMonth(e, 'right')}/>
-      </div>
       <CalendarFrame
         month={monthTable}
         numRows={H.numberOfRowsForMonthTable(monthTable)}
