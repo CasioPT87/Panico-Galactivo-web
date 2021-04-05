f3b9fdf09395fc048bcbe40f8cd2fd084be08382
Sesko <sergioibanezmoreno@gmail.com>
2021-04-05 15:52:12 +0100

    removed fixed size for box

--- src/components/calendar/calendar-month/CalendarMonth.js
+++ src/components/calendar/calendar-month/CalendarMonth.js
@@ -4,17 +4,15 @@ import Box from '../box/Box';
 import CalendarFrame from '../calendar-frame/CalendarFrame'
 
 export default function({ monthTable, selectDate, selectedDate }) {
-  return (
-    <div>
-      <CalendarFrame
-        month={monthTable}
-        numRows={H.numberOfRowsForMonthTable(monthTable)}
-        tableDays={H.dayObjectsForMonthTable(monthTable)}
-        weekLength={H.numberOfDaysWeek()}
-      >
-        {(index, dayData) => <Box index={index} dayData={dayData} selectedDate={selectedDate} selectDate={selectDate} />}
-      </CalendarFrame>
-    </div>
+  return ( 
+    <CalendarFrame
+      month={monthTable}
+      numRows={H.numberOfRowsForMonthTable(monthTable)}
+      tableDays={H.dayObjectsForMonthTable(monthTable)}
+      weekLength={H.numberOfDaysWeek()}
+    >
+      {(index, dayData) => <Box index={index} dayData={dayData} selectedDate={selectedDate} selectDate={selectDate} />}
+    </CalendarFrame>
   );
 
 }
