f1007f7fbc41249d37da16fa4ca020ef422b49a9
Sesko <sergioibanezmoreno@gmail.com>
2021-04-05 11:25:48 +0100

    Calendar converted to functional component

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,55 +1,49 @@
-import React from 'react';
+import React, { useState }from 'react';
 import moment from 'moment';
+import { useSpring, animated } from 'react-spring'
 import CalendarMonth from './calendar-month/CalendarMonth';
 import H from './helper/CalendarHelper';
 import styles from './Calendar.module.css';
 
 const POSITIONS = ['left', 'center', 'right'];
-export default class Calendar extends React.PureComponent {
+export default function() {
 
-  state = {
-    position: 'center',
-    monthTable: moment().month(),
-    selectedDate: {
-      day: moment().date(),
-      month: moment().month(),
-      year: moment().year(),
-      dayOfWeek: moment().day()
-    }
-  }
+  const [selectedDate, setSelectedDate] = useState({
+    day: moment().date(),
+    month: moment().month(),
+    year: moment().year(),
+    dayOfWeek: moment().day()
+  });
 
-  selectDate = () => {
+  const [position, setPosition] = useState('center');
+  const [monthTable, setMonthTable] = useState(moment().month());
+
+  const selectDate = () => {
     console.log('we are selecting a date')
   }
 
-  goTo = (direction) => {
-    const { position } = this.state;
+  const goTo = (direction) => {
     let positionIndex = POSITIONS.findIndex(pos => pos === position);
     if (positionIndex < 0 || position > (POSITIONS.length - 1)) return;
     if (direction === 'left') positionIndex--;
     if (direction === 'right') positionIndex++;
     if (!!POSITIONS[positionIndex]) {
-      this.setState({ position: POSITIONS[positionIndex] });
+      setPosition(POSITIONS[positionIndex]);
     }
   }
 
-  calendarWindowStyle = () => {
-    const { position } = this.state;
+  const calendarWindowStyle = () => {
     const styleClass = `c_calendar--${position}`
     return styles[styleClass];
   }
 
-  render() {
-    const { monthTable, position, selectedDate } = this.state;
-
-    return (
-      <div className={styles.c_calendar__wrapper}>
-        <div className={this.calendarWindowStyle()}>
-          <CalendarMonth monthTable={H.getPreviousMonth(monthTable)} selectedDate={selectedDate} selectDate={this.selectDate} goTo={this.goTo}/>
-          <CalendarMonth monthTable={monthTable} selectedDate={selectedDate} selectDate={this.selectDate} goTo={this.goTo}/>
-          <CalendarMonth monthTable={H.getNextMonth(monthTable)} selectedDate={selectedDate} selectDate={this.selectDate} goTo={this.goTo}/>
-        </div>
+  return (
+    <div className={styles.c_calendar__wrapper}>
+      <div className={calendarWindowStyle()}>
+        <CalendarMonth monthTable={H.getPreviousMonth(monthTable)} selectedDate={selectedDate} selectDate={selectDate} goTo={goTo}/>
+        <CalendarMonth monthTable={monthTable} selectedDate={selectedDate} selectDate={selectDate} goTo={goTo}/>
+        <CalendarMonth monthTable={H.getNextMonth(monthTable)} selectedDate={selectedDate} selectDate={selectDate} goTo={goTo}/>
       </div>
-    );
-  }
+    </div>
+  );
 }
