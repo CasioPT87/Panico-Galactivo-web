924ca212822c73ea1c14bd307895848661692ec7
Sesko <sergioibanezmoreno@gmail.com>
2021-04-07 07:00:00 +0100

    red for sundays

--- src/components/calendar/box/Box.js
+++ src/components/calendar/box/Box.js
@@ -1,8 +1,15 @@
 import styles from './Box.module.css'
 
 export default function({ dayData, selectedDate }) {
+  const isSunday = () => {
+    return dayData.date.day() === 0;
+  }
   return (
-    <div className={`${styles.c_boxes__item} ${dayData.inactive ? styles['c_boxes__item--inactive'] : ''}`}>
+    <div className={`
+      ${styles.c_boxes__item}
+      ${dayData.inactive ? styles['c_boxes__item--inactive'] : ''}
+      ${isSunday() ? styles['c_boxes__item--sunday'] : ''}
+      `}>
       {dayData.info.dayDisplay}
     </div>
   )
